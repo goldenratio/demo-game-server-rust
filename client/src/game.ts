e25d5785aead2695eb4f304290d0b0c92e029efa
karthikeyan.jayakuma <karthikeyan.jayakumar@gamesys.ee>
2024-04-17 14:55:04 +0300

    more crap

--- client/src/game.ts
+++ client/src/game.ts
@@ -15,7 +15,7 @@ const app = new Application({
 	width: 800,
 	height: 600,
 	backgroundColor: 0x1099bb,
-	resolution: window.devicePixelRatio || 1,
+	resolution: 1,
 	sharedTicker: true,
 	hello: true,
 });
