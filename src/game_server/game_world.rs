c247bdd14881828d29eca5aaa5701504db6bfbe4
karthikeyan.jayakuma <karthikeyan.jayakumar@gamesys.ee>
2024-04-25 17:23:50 +0300

    more crap

--- src/game_server/game_world.rs
+++ src/game_server/game_world.rs
@@ -3,18 +3,23 @@ use std::collections::HashMap;
 #[derive(Debug, Copy, Clone)]
 pub struct PeerPlayerInfo {
     pub player_id: usize,
+    pub display_name: String,
     pub x: f32,
     pub y: f32
 }
 
 #[derive(Debug)]
 pub struct GameWorld {
+    max_players_count: u8,
+    current_players_count: u8,
     player_map: HashMap<usize, PeerPlayerInfo>,
 }
 
 impl Default for GameWorld {
     fn default() -> Self {
         Self {
+            max_players_count: 2,
+            current_players_count: 0,
             player_map: HashMap::new()
         }
     }
@@ -22,15 +27,28 @@ impl Default for GameWorld {
 
 impl GameWorld {
     pub fn add_player(&mut self, player_id: usize) {
-        self.player_map.insert(player_id, PeerPlayerInfo {
+        let peer_data = PeerPlayerInfo {
+            display_name: "no_name".to_string(),
             player_id,
             x: 0.0,
             y: 0.0
-        });
+        };
+
+        if self.current_players_count >= self.max_players_count {
+            // room is full
+            return;
+        }
+
+        if let Some(_) = self.player_map.insert(player_id, peer_data) {
+            self.current_players_count += 1;
+        };
     }
 
     pub fn remove_player(&mut self, player_id: usize) {
-        self.player_map.remove(&player_id);
+        if let Some(_) = self.player_map.remove(&player_id) {
+            self.current_players_count -= 1;
+            // remove this game world is player count is 0
+        }
     }
 
     pub fn update_player_position(&mut self, player_id: usize, x: f32, y: f32) {
